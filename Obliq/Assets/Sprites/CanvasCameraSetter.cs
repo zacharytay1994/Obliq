2204d1676001e0a0ebd4e2ae1c77e7b2578f1a89
Steef <steven.koe80@gmail.com>
2019-10-29 14:04:09 +0800

    Revert "Change 1"
    This reverts commit 92b612b5bde169e5095437d63ccbea1e7fa879e9.

--- Obliq/Assets/Sprites/CanvasCameraSetter.cs
+++ Obliq/Assets/Sprites/CanvasCameraSetter.cs
@@ -10,7 +10,7 @@ public class CanvasCameraSetter : MonoBehaviour
     void Start()
     {
         current_canvas = GetComponent<Canvas>();
-        current_camera = GameObject.FindGameObjectWithTag("GlowCamera").GetComponent<Camera>();
+        current_camera = Camera.main;
 
     }
 
@@ -22,7 +22,7 @@ public class CanvasCameraSetter : MonoBehaviour
             if (Camera.main != null)
             {
                 current_camera = Camera.main;
-                current_canvas.worldCamera = GameObject.FindGameObjectWithTag("GlowCamera").GetComponent<Camera>();
+                current_canvas.worldCamera = current_camera;
                 Debug.Log("Loading Scene Camera Set!");
             }
             else
