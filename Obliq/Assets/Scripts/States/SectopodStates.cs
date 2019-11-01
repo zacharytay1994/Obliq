3a289051c754b405070ada87ec692d45c73a2d3e
Tristan Cheah <cheahtristan@hotmail.sg>
2019-11-01 21:22:20 +0800

    Starting sectopod
    Sectopod now aggros when good guy is within range. Implementing attack behaviour next

---
+++ Obliq/Assets/Scripts/States/SectopodStates.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using static GF;
+
+public class SectopodStates 
+{
+   
+}
+public class SectopodIdleState : State
+{
+    GameObject closest_obj;
+    public override void Enter(GameObject owner)
+    {
+    }
+    public override void Execute(GameObject owner)
+    {
+        closest_obj = owner.GetComponent<Entity>().world_handler_reference_.GetNearestGoodGuy(owner.transform.position);
+        owner.GetComponent<ObliqPathfinding>().target_ = closest_obj.transform.position;
+
+        if ((closest_obj.transform.position - owner.transform.position).magnitude < GC<Entity>(owner).GetTrueRange())// if within range
+        {
+            owner.GetComponent<Entity>().statemachine_.ChangeState(new SectopodAttackState());
+        }
+
+    }
+    public override void Exit(GameObject owner)
+    {
+    }
+}
+public class SectopodAttackState : State
+{
+    public override void Enter(GameObject owner)
+    {
+        Debug.Log("Attack state");
+    }
+    public override void Execute(GameObject owner)
+    {
+    }
+    public override void Exit(GameObject owner)
+    {
+    }
+}
