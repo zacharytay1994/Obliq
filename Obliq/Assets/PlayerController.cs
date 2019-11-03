20bc2f149f9d827e0c16443712b66d988d682457
Ged <wshjedidiah@gmail.com>
2019-11-03 13:24:50 +0800

    player movement

---
+++ Obliq/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera camera_;
+    [SerializeField] float player_speed_;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerFacingDirection();
+
+        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.0f, player_speed_, 0.0f);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0.0f, player_speed_, 0.0f);
+        }
+    }
+
+    float PlayerFacingDirection()
+    {
+        Vector2 mouseLocation = Input.mousePosition;
+        mouseLocation = camera_.ScreenToWorldPoint(mouseLocation);
+        float angle = AngleBetween(transform.position, mouseLocation);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        return angle;
+    }
+
+    float AngleBetween(Vector2 a, Vector2 b)
+    {
+        return Mathf.Atan2(a.y-b.y, a.x-b.x )*Mathf.Rad2Deg + 90;
+    }
+}
