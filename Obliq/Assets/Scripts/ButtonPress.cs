d8cd78d3bc7f44bb43639272675af8cb520c0274
Steef <steven.koe80@gmail.com>
2019-11-04 15:57:18 +0800

    Fire Jed Button Fires Jed now

--- Obliq/Assets/Scripts/ButtonPress.cs
+++ Obliq/Assets/Scripts/ButtonPress.cs
@@ -15,11 +15,15 @@ public class ButtonPress : MonoBehaviour
     [SerializeField] float pauseSecondsBeforeLoad;
     float internaltimer;
     SceneTransitionLoader STM_;
-
     IsButtonClicked isclicked;
 
+    [Space(10)]
     [SerializeField] string sceneToLoad;
     bool isMouseOver;
+
+    [Space(10)]
+    [SerializeField] bool OptionButton;
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,38 +38,47 @@ public class ButtonPress : MonoBehaviour
     void Update()
     {
         
-        
-
         if (!isclicked.isButtonClicked)
         {
             if (Input.GetMouseButtonDown(0))
             {
                 if (isMouseOver)
                 {
-                    anim.SetBool("IsClicked", true);
-                    buttonAnim.SetTrigger("SceneEnd");
-                    
-
-                    if (STM_)
+                    if (!OptionButton) //Non Option Buttons Code
                     {
-                        Instantiate(spawnParticle, transform.position, new Quaternion(0, 0, 0, 0));
-                        if (sceneToLoad != "Exit")
+                        anim.SetBool("IsClicked", true);
+                        buttonAnim.SetTrigger("SceneEnd");
+
+
+                        if (STM_)
                         {
-                            _AM.PlaySound("WAHH");
-                            buttonAnim.SetTrigger("SceneEnd");
-                            
+                            Instantiate(spawnParticle, transform.position, new Quaternion(0, 0, 0, 0));
+                            if (sceneToLoad != "Exit")
+                            {
+                                _AM.PlaySound("WAHH");
+                                buttonAnim.SetTrigger("SceneEnd");
+
+                            }
+                            else
+                            {
+                                Application.Quit();
+                            }
                         }
                         else
                         {
-                            Application.Quit();
+                            Debug.Log("No scenemanager in scene. Launch game from splashscreen");
+                        }
+
+                        isclicked.isButtonClicked = true;
+                    }
+                    else //Option Button Code
+                    {
+                        if (STM_)
+                        {
+                            Instantiate(spawnParticle, transform.position, new Quaternion(0, 0, 0, 0));
+                            _AM.PlaySound("WAHH");
                         }
                     }
-                    else
-                    {
-                        Debug.Log("No scenemanager in scene. Launch game from splashscreen");
-                    }
-
-                    isclicked.isButtonClicked = true;
                 }
             }
         }
@@ -89,7 +102,6 @@ public class ButtonPress : MonoBehaviour
     {
         if (isclicked.turnon && !isclicked.isButtonClicked)
         {
-            
             //If your mouse hovers over the GameObject with the script attached, output this message
             _AM.PlaySound("Wahh");
             isMouseOver = true;
@@ -103,7 +115,6 @@ public class ButtonPress : MonoBehaviour
     {
         if (isclicked.turnon && !isclicked.isButtonClicked)
         {
-            
             //The mouse is no longer hovering over the GameObject so output this message each frame
             isMouseOver = false;
             anim.SetBool("IsHover", isMouseOver);
