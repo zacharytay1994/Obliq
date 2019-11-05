39ee2e538a182ecb31566e876c6e1bdc2bc47fa8
Tristan Cheah <cheahtristan@hotmail.sg>
2019-11-05 10:59:13 +0800

    Adapted charger to match real time proto type
    Charger is now ok, may need some tweaking, enemy prefabs created

--- Obliq/Assets/Scripts/EntityScripts/Charger.cs
+++ Obliq/Assets/Scripts/EntityScripts/Charger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Charger : MonoBehaviour
 {
     // enemy variables
+    bool hasCollided = false;
     public float move_distance_ = 10.0f;
     public GameObject target_reference_;
     // entity reference
@@ -16,7 +17,17 @@ public class Charger : MonoBehaviour
         entity_reference_ = gameObject.GetComponent<Entity>();
         entity_reference_.statemachine_.SetState(new ChargerIdleState());
     }
-
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        hasCollided = true;   
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (hasCollided == true)
+            hasCollided = false;
+            entity_reference_.statemachine_.SetState(new ChargerIdleState());        
+    }
+   
     // Update is called once per frame
     void Update()
     {
