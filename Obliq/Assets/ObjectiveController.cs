e7194b3dd95060362b63e23d36737063467d8c53
Tristan Cheah <cheahtristan@hotmail.sg>
2019-11-05 09:31:43 +0800

    Objective
    Objective changed to if release before defuse timer, timer will reset

--- Obliq/Assets/ObjectiveController.cs
+++ Obliq/Assets/ObjectiveController.cs
@@ -20,31 +20,34 @@ public class ObjectiveController : MonoBehaviour
         defuseBomb();
     }
     bool defuseBomb()
-    {
+    {      
         if (Time.time - start_time_ >= 10 && defused == false)
         {
             Debug.Log("DEAD");
             return defused;
             //Application.Quit;
         }
-       // if ((GameObject.Find("Entities/GoodGuys/GoodGuy").transform.position - gameObject.transform.position).magnitude < 2.0f)
-       // {
-
-        if (Input.GetKey(KeyCode.Q))
+        if ((GameObject.Find("Entities/GoodGuys/GoodGuy").transform.position - GameObject.Find("Entities/Objective").transform.position).magnitude < 3.0f)
         {
-            Debug.Log(defuse_timer += Time.deltaTime);
-            defuse_timer += Time.deltaTime;
-        }
-        if (Input.GetKeyUp(KeyCode.Q))
-        {
-            if (defuse_timer > 2.0f)
+            if (Input.GetKey(KeyCode.Q))
             {
-                Debug.Log("Alive");
-                defused = true;
-                return defused;
+                Debug.Log(defuse_timer += Time.deltaTime);
+                defuse_timer += Time.deltaTime;
             }
-        }          
-       // }
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+                if (defuse_timer > 2.0f)
+                {
+                    Debug.Log("Alive");
+                    defused = true;
+                    return defused;
+                }
+                else
+                {
+                    defuse_timer = 0.0f;
+                }
+            }          
+        }
         return defused;
     }
 }
