8697ab182278abcb901005ab10c94aa728cfa8f3
Ged <wshjedidiah@gmail.com>
2019-11-14 22:52:42 +0800

    screenshake + hitpause on hit

--- Obliq/Assets/Attacks/WeaponsStash/DamageEnemy.cs
+++ Obliq/Assets/Attacks/WeaponsStash/DamageEnemy.cs
@@ -10,12 +10,22 @@ public class DamageEnemy : MonoBehaviour
     LayerMask enemy_mask_ = 0;
     [SerializeField]
     bool causes_hit_pause_ = false;
+    [SerializeField]
+    bool causes_screen_shake_ = false;
+    [SerializeField]
+    float screen_shake_base_duration_;
+    [SerializeField]
+    float screen_shake_base_magnitude_;
+
+
 
     HitPause hit_pause_;
+    CameraManager camera_manager_;
     // Start is called before the first frame update
     void Start()
     {
         hit_pause_ = FindObjectOfType<HitPause>();
+        camera_manager_ = FindObjectOfType<CameraManager>();
     }
 
     // Update is called once per frame
@@ -30,10 +40,16 @@ public class DamageEnemy : MonoBehaviour
         {
             if (collision.gameObject.GetComponent<HealthComponent>() != null)
             {
-                if(causes_hit_pause_)
+                if (causes_screen_shake_)
+                {
+                    camera_manager_.Shake(hit_pause_.base_duration_ * damage_ * screen_shake_base_duration_, damage_ * screen_shake_base_magnitude_);
+                }
+                if (causes_hit_pause_)
                 {
                     hit_pause_.Freeze(damage_);
                 }
+
+
                 collision.gameObject.GetComponent<HealthComponent>().TakeDamage(damage_);
                 // get direction
                 Vector2 direction = ((Vector2)collision.gameObject.transform.position - (Vector2)transform.position).normalized;
