d98b4a13abb9760c7db7d0ebdb64cea1355b4a97
Ged <wshjedidiah@gmail.com>
2019-12-16 18:09:30 +0800

    changes 20 days ago so I forgot most of it
    enemy spawn timer, exit room class that is empty,

--- Obliq/Assets/Attacks/WeaponsStash/SpawningScript.cs
+++ Obliq/Assets/Attacks/WeaponsStash/SpawningScript.cs
@@ -9,27 +9,38 @@ public class SpawningScript : MonoBehaviour
     int max_grunt_count_ = 10;
     [SerializeField]
     GameObject grunt_ = null;
+
+    [SerializeField]
+    float grunt_spawn_duration_ = 0.0f;
+
+    float grunt_spawn_timer_ = 0;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         SpawnGrunt();
+        if(grunt_spawn_timer_<= grunt_spawn_duration_)
+        {
+            grunt_spawn_timer_ += Time.timeScale;
+        }   
     }
 
     void SpawnGrunt()
     {
         if (grunt_ != null)
         {
-            if (grunt_count_ < max_grunt_count_)
+            if (grunt_count_ < max_grunt_count_ && grunt_spawn_timer_>=grunt_spawn_duration_)
             {
                 GameObject temp = Instantiate(grunt_, new Vector3(transform.position.x, transform.position.y, -1.0f) + new Vector3(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 0.0f), Quaternion.identity);
                 temp.GetComponent<TempGrunt>().AttachSpawner(this);
                 grunt_count_++;
+                grunt_spawn_timer_ = 0;
             }
         }
     }
@@ -37,5 +48,6 @@ public class SpawningScript : MonoBehaviour
     public void GruntDied()
     {
         grunt_count_ -= 1;
+        grunt_spawn_timer_ = 0;
     }
 }
