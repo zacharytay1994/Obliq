be74fcdebeda0c601b55380e7224bc09037dfcb7
Ged <wshjedidiah@gmail.com>
2019-12-26 17:35:21 +0800

    minor changes to decision room

---
+++ Obliq/Assets/MoveToNewRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToNewRoom : MonoBehaviour
+{
+    //get room database
+    int room_database_size_ = 0;
+    int room_number_ = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        room_number_ = Random.Range(0, room_database_size_);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //loads room number
+    }
+}
