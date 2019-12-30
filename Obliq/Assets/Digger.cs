e9db6826a24124eefc338b6580fd92d8a6620fa0
Tristan Cheah <cheahtristan@hotmail.sg>
2019-12-30 12:10:11 +0800

    Experimental enemy

---
+++ Obliq/Assets/Digger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Digger : MonoBehaviour
+{
+    Entity entity_reference_;
+    HealthComponent health_;
+    public GameObject player;
+   
+    [SerializeField]
+    GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entity_reference_ = gameObject.GetComponent<Entity>();
+        entity_reference_.statemachine_.SetState(new DiggerIdleState());
+        health_ = gameObject.GetComponent<HealthComponent>();
+        player = GameObject.Find("Player");
+        Physics.IgnoreLayerCollision(15, 17);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FireBullets()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            GameObject weapon_bullet = Instantiate(bullet, gameObject.transform.GetChild(0).gameObject.transform.position, Quaternion.identity);
+            weapon_bullet.GetComponent<ImAProjectile>().InitProj();
+        }
+
+    }
+}
