3c39664912483af8a3fd8990298e2d7ea11718c5
Ged <wshjedidiah@gmail.com>
2020-01-07 23:11:42 +0800

    modified tutorial, added screenshake and sound to chargers

--- Obliq/Assets/PlaySoundOnCollide.cs
+++ Obliq/Assets/PlaySoundOnCollide.cs
@@ -7,12 +7,29 @@ public class PlaySoundOnCollide : MonoBehaviour
     AudioManager am_;
     [SerializeField] string sound_;
     [SerializeField] int layer_;
-    
+    [SerializeField] float damaged_timer_;
+    SpriteRenderer sr_;
+    float timer_ = 0;
+
     // Start is called before the first frame update
     void Awake()
     {
         am_ = FindObjectOfType<AudioManager>();
+        sr_ = GetComponent<SpriteRenderer>();
        
+    }
+
+    private void Update()
+    {
+        if (timer_>0)
+        {
+            timer_ -= Time.deltaTime;
+            sr_.color = new Color(sr_.color.r, sr_.color.b, sr_.color.g, 0.5f);
+        }
+        else
+        {
+            sr_.color = new Color(sr_.color.r, sr_.color.b, sr_.color.g, 1f);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -20,6 +37,7 @@ public class PlaySoundOnCollide : MonoBehaviour
         if(collision.collider.gameObject.layer == layer_)
         {
             am_.PlaySound(sound_);
+            timer_ = damaged_timer_;
         }
     }
 
