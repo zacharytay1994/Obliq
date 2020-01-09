c085fa597ab8f8fe22ce11cb2e820f68ef72e151
Ged <wshjedidiah@gmail.com>
2020-01-09 22:55:00 +0800

    shooting tooltip only appears when gun is picked up

---
+++ Obliq/Assets/OnlyAppearWhenItemDissapears.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyAppearWhenItemDissapears : MonoBehaviour
+{
+    [SerializeField]
+    GameObject disappear_item_;
+    bool exists_at_start_;
+
+    [SerializeField]
+    List<GameObject> activate_items_;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(disappear_item_!=null)
+        {
+            exists_at_start_ = true;
+        }
+        else
+        {
+            exists_at_start_ = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(disappear_item_ == null)
+        {
+            foreach(GameObject g in activate_items_)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+
+}
