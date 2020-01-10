c221ae78087058b054851a943c97dd03a7d482a5
zacharytay1994 <s10179991@connect.np.edu.sg>
2020-01-10 13:13:23 +0800

    not using assetdatabase

--- Obliq/Assets/Prefabs/UI/GimmeTooltip.cs
+++ Obliq/Assets/Prefabs/UI/GimmeTooltip.cs
@@ -15,12 +15,17 @@ public class GimmeTooltip : MonoBehaviour
     float hover_scale = 0.2f;
     [SerializeField]
     Color color_ = new Color(1.0f, 1.0f, 1.0f);
+    [SerializeField]
+    GameObject tool_tip_ = null;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject g = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/UI/ToopTip.prefab", typeof(GameObject));
-        GameObject temp = Instantiate(g);
-        temp.GetComponent<ToopTipControl>().Init(gameObject, text_, y_offset_, hover_speed_, hover_scale, color_);
+        //GameObject g = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/UI/ToopTip.prefab", typeof(GameObject));
+        if (tool_tip_ != null)
+        {
+            GameObject temp = Instantiate(tool_tip_);
+            temp.GetComponent<ToopTipControl>().Init(gameObject, text_, y_offset_, hover_speed_, hover_scale, color_);
+        }
     }
 
     // Update is called once per frame
