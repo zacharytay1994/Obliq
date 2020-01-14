5a6ff11bdbc5e736bde8e5a5309c3009e4fb0a97
Ged <wshjedidiah@gmail.com>
2020-01-15 05:07:56 +0800

    objective text appears at bottom of player UI after it fades

--- Obliq/Assets/Prefabs/UI/ObjectiveTextControl.cs
+++ Obliq/Assets/Prefabs/UI/ObjectiveTextControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectiveTextControl : MonoBehaviour
 {
@@ -10,12 +11,16 @@ public class ObjectiveTextControl : MonoBehaviour
     float life_span_ = 3.0f;
     [SerializeField]
     float fade_speed_ = 1.0f;
+    [SerializeField]
+    TextMeshProUGUI bottom_text_;
     bool to_destroy_ = false;
     TMPro.TextMeshProUGUI tmp_ = null;
     // Start is called before the first frame update
     void Start()
     {
         tmp_ = GetComponent<TMPro.TextMeshProUGUI>();
+        bottom_text_.text = text_;
+        bottom_text_.color = new Color(1, 1, 1, 0);
         tmp_.text = text_;
     }
 
@@ -26,15 +31,22 @@ public class ObjectiveTextControl : MonoBehaviour
         if (to_destroy_)
         {
             Color temp = tmp_.color;
+            Color bottom_temp = bottom_text_.color;
             if (temp.a > 0.0f)
             {
                 temp.a -= fade_speed_ * Time.deltaTime;
+                
             }
             else
             {
                 GameObject.Destroy(gameObject);
             }
+            if(bottom_temp.a<1.0f)
+            {
+                bottom_temp.a += fade_speed_ * Time.deltaTime;
+            }
             tmp_.color = temp;
+            bottom_text_.color = bottom_temp;
         }
 
         // if life span up, mark to destroy
