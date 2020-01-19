3c4e4bb0bd43632b73fea985093fad1d4f133010
Ged <wshjedidiah@gmail.com>
2020-01-20 01:50:35 +0800

    the sound sounds better i think??

--- Obliq/Assets/ScrollingTextSystem.cs
+++ Obliq/Assets/ScrollingTextSystem.cs
@@ -11,8 +11,11 @@ public class ScrollingTextSystem : MonoBehaviour
     float scroll_delay_timer_;
     AudioManager am_;
     [Space(10)]
-    [SerializeField] string typing_sound_;
-
+    List<string> typing_sound_ = new List<string> { "kb3", "kb4", "kb5", "kb6" };
+    bool flicker_audio_ = true;
+    [SerializeField] bool chain_scrolling_text_;
+    [SerializeField] ScrollingTextSystem prev_text_chain_;
+    [HideInInspector] public bool scroll_complete_ = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +24,50 @@ public class ScrollingTextSystem : MonoBehaviour
         current_text_ = current_text_mesh_.text;
         current_text_mesh_.text = "";
         scroll_delay_timer_ = scroll_delay_;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        scroll_delay_timer_ -= Time.deltaTime;
-        if(scroll_delay_timer_<=0 && current_text_.Length>0)
+        if(chain_scrolling_text_)
         {
-            float random_pitch = Random.Range(0.7f, 1.3f);
-            am_.PlaySound(typing_sound_,1, random_pitch);
+            if(prev_text_chain_.scroll_complete_)
+            {
+                ScrollingText();
+            }
+        }
+        else
+        {
+            ScrollingText();
+        }
+        
+        if(current_text_.Length<=0)
+        {
+            scroll_complete_ = true;
+        }
+    }
+
+    void ScrollingText()
+    {
+        scroll_delay_timer_ -= Time.deltaTime;
+        if (scroll_delay_timer_ <= 0 && current_text_.Length > 0)
+        {
+
+                float random_pitch = Random.Range(0.7f, 1.3f);
+                int random_sound = Random.Range(0, typing_sound_.Count);
+
+                am_.PlaySound(typing_sound_[random_sound], 1, random_pitch);
+                Debug.Log(typing_sound_[random_sound]);
+            
+
+
             char c = current_text_[0];
             current_text_ = current_text_.Remove(0, 1);
             current_text_mesh_.text += c;
             scroll_delay_timer_ = scroll_delay_;
+
+            flicker_audio_ = !flicker_audio_;
         }
     }
 }
