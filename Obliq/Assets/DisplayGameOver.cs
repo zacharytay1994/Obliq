2ba0f50bb9502600cca8305e2305fe207efce074
Ryan Tan <ryantan1337@gmail.com>
2020-01-22 16:09:40 +0800

    some changes

--- Obliq/Assets/DisplayGameOver.cs
+++ Obliq/Assets/DisplayGameOver.cs
@@ -14,6 +14,8 @@ public class DisplayGameOver : MonoBehaviour
 
     SceneTransitionLoader stl_;
 
+    GameObject player_;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,7 @@ public class DisplayGameOver : MonoBehaviour
         text_.enabled = false;
         to_menu_button_.SetActive(false);
         hp_ = GameObject.Find("Player").GetComponent<HealthComponent>();
+        player_ = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -32,6 +35,7 @@ public class DisplayGameOver : MonoBehaviour
         {
             to_menu_button_.SetActive(true);
             text_.enabled = true;
+            player_.GetComponent<PlayerController>().SetAcceleration(0.0f);
         }
     }
 
