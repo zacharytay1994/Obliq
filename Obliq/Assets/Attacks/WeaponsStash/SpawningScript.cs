cf09f7c560bfdf67d57d3a519fd00de2f2d1f2e6
Ged <wshjedidiah@gmail.com>
2020-01-30 02:02:50 +0800

    finally got max grunts to work
    I was in pain. I really was. And knowing that at the end of every triumph is yet another adversary, I find it tempting to drop everything and just give up. The only thing that keeps me alive at this point is the hopes other people have placed upon me. Yet, if I fail, and betray these hopes, what do I have left? I would be an empty husk of a human being. Kids, please don't be like me. Don't live for the sake of others. Live for yourself. So that when you inevitably fail, you can pick yourself back up, kowing that your hope and determination comes from yourself. Thank you for coming to my ted talk, I am rahjit and I am out of here.

--- Obliq/Assets/Attacks/WeaponsStash/SpawningScript.cs
+++ Obliq/Assets/Attacks/WeaponsStash/SpawningScript.cs
@@ -7,7 +7,7 @@ public class SpawningScript : MonoBehaviour
     GruntCountFromSpawners gcfs_;
     [SerializeField]
     int grunt_spawn_limit_=10;
-    [HideInInspector] public int grunt_count_ = 0;
+    public int grunt_count_ = 0;
     [SerializeField]
     int max_grunt_count_ = 10;
     [SerializeField]
@@ -36,10 +36,7 @@ public class SpawningScript : MonoBehaviour
     {
         if (per_wave_)
         {
-            if(gcfs_.total_grunt_count_<=grunt_spawn_limit_-max_grunt_count_)
-            {
-                    SpawnWave();
-            }
+             SpawnWave(); 
         }
         else
         {
@@ -61,7 +58,6 @@ public class SpawningScript : MonoBehaviour
             if (grunt_count_ < max_grunt_count_ && waves_ > 0)
             {
                 GameObject temp = Instantiate(grunt_, new Vector3(transform.position.x + Random.Range(random_offset_.x, random_offset_.y), transform.position.y + Random.Range(random_offset_.x, random_offset_.y), -1.0f) + new Vector3(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 0.0f), Quaternion.identity);
-                temp.transform.SetParent(GameObject.Find("Enemy").transform);
                 if (temp.GetComponent<TempGrunt>() != null)
                 {
                     temp.GetComponent<TempGrunt>().AttachSpawner(this);
@@ -86,7 +82,7 @@ public class SpawningScript : MonoBehaviour
 
     void SpawnWave()
     {
-        if (spawn_interval_counter_ > spawn_interval_)
+        if (spawn_interval_counter_ > spawn_interval_ && gcfs_.total_grunt_count_< grunt_spawn_limit_)
         {
             spawn_interval_counter_ = 0;
             if (grunt_ != null && waves_ > 0)
@@ -104,6 +100,7 @@ public class SpawningScript : MonoBehaviour
                         temp.GetComponent<GruntSpawnAnimation>().SetSpawner(gameObject);
                         temp.GetComponent<GruntSpawnAnimation>().Init();
                     }
+                    grunt_count_++;
                 }
             }
             waves_--;
