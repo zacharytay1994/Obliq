fda20aa7937898457f54fa6531d471c92eb60860
Wiinterr <55229254+Wiinterr@users.noreply.github.com>
2020-01-31 23:29:31 +0800

    Added glow on hover

---
+++ Obliq/Assets/Prefabs/UI/GlowOnHover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlowOnHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        gameObject.GetComponent<Text>().fontStyle = FontStyle.Bold;
+        Debug.Log("Gay");
+    }
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<Text>().fontStyle = FontStyle.Normal;
+        Debug.Log("No U");
+    }
+}
