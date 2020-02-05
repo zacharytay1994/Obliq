4b817a2fa7a897c970fcd7cf3d27fe177fe21957
Austin Hwang <hwangjingrui@gmail.com>
2020-02-05 12:19:09 +0800

    removed some unnecessary stuff, fixed null error in worldhandler script

--- Obliq/Assets/Scripts/World/WorldHandler.cs
+++ Obliq/Assets/Scripts/World/WorldHandler.cs
@@ -4,35 +4,27 @@ using UnityEngine;
 
 public class WorldHandler : MonoBehaviour
 {
-   // GameObject radius_one_;
-   // GameObject radius_two_;
-   // GameObject radius_three;
     public  List<GameObject> enemies_ = new List<GameObject>();
     public  List<GameObject> goodguys_ = new List<GameObject>();
     bool has_enemies_ = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        // radius_one_ = GameObject.Find("RadiusOne");
-        // radius_two_ = GameObject.Find("RadiusTwo");
-        // radius_three = GameObject.Find("RadiusThree");
         if (GameObject.FindGameObjectsWithTag("Enemy").Length > 0)
         {
-            foreach (Transform t in GameObject.Find("Enemy").transform)
+            if (GameObject.Find("Enemy") != null)
             {
-                Debug.Log(t.gameObject);
-                enemies_.Add(t.gameObject);
+                foreach (Transform t in GameObject.Find("Enemy").transform)
+                {
+                    Debug.Log(t.gameObject);
+                    enemies_.Add(t.gameObject);
+                }
             }
         }
-        else has_enemies_ = false; 
+        else has_enemies_ = false;
 
-        //foreach (Transform t in GameObject.Find("Player").transform)
-        //{
-        //    Debug.Log(t.gameObject);
-        //    goodguys_.Add(t.gameObject);
-        //}
         goodguys_.Add(GameObject.Find("Player"));
-       // SetMoveRadiusActive(false);
     }
 
     // Update is called once per frame
